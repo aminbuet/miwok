43d4e436c9d429145d34abfc9c92d7a895f86f5a
Md Aminur Rahman <aminbuet@gmail.com>
2017-09-27 23:27:57 +1000

    Added Colors

---
+++ app/src/main/java/com/azan/miwok/ColorsClickListener.java
@@ -0,0 +1,19 @@
+package com.azan.miwok;
+
+
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Md Aminur Rahman on 23/9/17.
+ */
+
+public class ColorsClickListener implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view){
+        Toast.makeText(view.getContext(),
+                "Open List of numbers",Toast.LENGTH_SHORT).show();
+
+    }
+}
