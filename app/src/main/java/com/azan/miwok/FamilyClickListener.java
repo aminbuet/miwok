db7648e5a48461d6fc0b5d005fef26341d553edd
Md Aminur Rahman <aminbuet@gmail.com>
2017-09-27 23:38:04 +1000

    Added Family Members

---
+++ app/src/main/java/com/azan/miwok/FamilyClickListener.java
@@ -0,0 +1,18 @@
+package com.azan.miwok;
+
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Md Aminur Rahman on 23/9/17.
+ */
+
+public class FamilyClickListener implements View.OnClickListener{
+
+    @Override
+    public void onClick(View view){
+        Toast.makeText(view.getContext(),
+                "Open List of numbers",Toast.LENGTH_SHORT).show();
+
+    }
+}
